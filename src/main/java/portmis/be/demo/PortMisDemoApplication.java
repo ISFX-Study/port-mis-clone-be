d6afbecff8159e5b43d56a30c580c0969a4959fe
HYEONA-KO <hyeona9312@naver.com>
2024-02-23 23:23:58 +0900

    init

---
+++ src/main/java/portmis/be/demo/PortMisDemoApplication.java
@@ -0,0 +1,13 @@
+package portmis.be.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortMisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortMisDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/portmis/be/demo/PortMisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package portmis.be.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortMisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
